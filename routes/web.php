c9707c6648e54122e87857800917b2187b89c1d3
AndreMaulanaMustofa <andremm73@gmail.com>
2022-03-07 23:45:41 +0700

    Praktikum_View

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/experiance', function () {
+    return view('experiance');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/single', function () {
+    return view('single');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
